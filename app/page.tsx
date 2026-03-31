7060d84b690a378614502e7ee42828e00a964f7c
Latheesh Poondla <latheeshpoondla1@gmail.com>
2026-03-31 22:37:54 +0530

    testing page edits

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="p-10">
       <h1 className="text-3xl font-bold mb-6">
-        🌱 Smart Plant Dashboard
+        🌱 Smart Plant Dashboard (Hi Hitesh)
       </h1>
 
       <Dashboard />
